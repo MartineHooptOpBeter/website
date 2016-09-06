8ef68943b0ce504e568988ddd18b58e7ec6b446e
Marco Miltenburg <marco@virtualpages.nl>
2016-09-06 18:17:52 +0200

    Footer toegevoegd.

--- src/index.php
+++ src/index.php
@@ -143,6 +143,10 @@
 		</div>
 	
 	</section>
+	
+	<footer>
+		<p>&copy <?php echo Date("Y"); ?> - Website gratis aangeboden door <a href="http://www.virtualpages.nl" target="_blank"><img src="<?php bloginfo('template_url'); ?>/img/virtualpages.svg" class="logo" alt="Virtual Pages" /></a></p>
+	</footer>
 
 </body>
 
