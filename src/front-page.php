9ef30f7d4cc8aed5a909db96d5d05190b487c5f9
Marco Miltenburg <marco@virtualpages.nl>
2016-09-11 21:37:02 +0200

    Sorteervolgorde aangepast zodat de volgorde van WordPress wordt gebruikt.

--- src/front-page.php
+++ src/front-page.php
@@ -55,7 +55,7 @@
 	<?php
 
 		// Get top level pages
-		query_posts( array( 'post_parent' => 0, 'post_type' => 'page', 'order' => 'ASC', 'post_per_page' => -1 ) );
+		query_posts( array( 'post_parent' => 0, 'post_type' => 'page', 'order' => 'ASC', 'orderby' => 'menu_order', 'post_per_page' => -1 ) );
 
 	?>
 
