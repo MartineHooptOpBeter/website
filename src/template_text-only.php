663777668f0187f1ff539ceb8892e4e5b6e48ad6
Marco Miltenburg <marco@virtualpages.nl>
2016-09-12 21:51:55 +0200

    Alle tekst in de content blokken is nu in een div met de class "text".

--- src/template_text-only.php
+++ src/template_text-only.php
@@ -6,8 +6,10 @@
 	<section class="content <?php echo $showDivider ? ' divider' : ''; ?>">
 		<div class="sitewidth clearfix">
 
-            <?php the_title( '<h2>', '</h2>' ); ?>
-            <?php the_content(); ?>
+			<div class="text">
+				<?php the_title( '<h2>', '</h2>' ); ?>
+				<?php the_content(); ?>
+			</div>
 
 		</div>
 	</section>
--- src/template_text-with-large-photo.php
+++ src/template_text-with-large-photo.php
@@ -6,15 +6,12 @@
 	<section class="content<?php echo $showDivider ? '  divider' : ''; ?>">
 		<div class="sitewidth">
 
-<?php
+			<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'martinehooptopbeter_fullwidth' ); } ?>
 
-    if ( has_post_thumbnail() ) { 
-        the_post_thumbnail( 'martinehooptopbeter_fullwidth' );
-    }
+			<div class="text">
+				<?php the_title( '<h2>', '</h2>' ); ?>
+				<?php the_content(); ?>
+			</div>
 
-    the_title( '<h2>', '</h2>' );
-    the_content();
-
-?>
 		</div>
 	</section>
