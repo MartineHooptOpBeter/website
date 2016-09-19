310b27314381ea26e1fdf5050bd930e3bace3b08
Marco Miltenburg <marco@virtualpages.nl>
2016-09-19 19:10:43 +0200

    Code toegevoegd om het formulier werkend te maken. De donatie moet nog wel in de database worden opgeslagen.

--- src/donate.php
+++ src/donate.php
@@ -3,6 +3,46 @@
     function show_donate_page($page = 1)
     {
 
+        $missingfields = [];
+
+    	if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+            $donate_name = isset($_POST['donate_name']) ? trim($_POST['donate_name']) : '';
+            $donate_email = isset($_POST['donate_email']) ? trim($_POST['donate_email']) : '';
+            $donate_message = isset($_POST['donate_message']) ? trim($_POST['donate_message']) : '';
+            $donate_amount = isset($_POST['donate_amount']) ? trim($_POST['donate_amount']) : 0;
+            $donate_payment_method = isset($_POST['donate_payment_method']) ? trim($_POST['donate_payment_method']) : '';
+            $donate_anonymous = isset($_POST['donate_anonymous']);
+            $donate_no_amount = isset($_POST['donate_no_amount']);
+
+            if (empty($donate_name)) {
+                $missingfields['donate_name'] = __('Required field', 'martinehooptopbeter');
+            }
+
+            if (empty($donate_email)) {
+                $missingfields['donate_email'] = __('Required field', 'martinehooptopbeter');
+            }
+
+            $donate_amount_parsed = parseAmount($donate_amount);
+            if ($donate_amount_parsed <= 0) {
+                $missingfields['donate_amount'] = __('Invalid amount', 'martinehooptopbeter');
+            }
+            if ($donate_amount_parsed < 500) {
+                $missingfields['donate_amount'] = __('Minimum required amount is 5 Euro', 'martinehooptopbeter');
+            }
+
+            if (($donate_payment_method != 'ideal') && ($donate_payment_method != 'creditcard')) {
+                $missingfields['donate_payment_method'] = __('Required field', 'martinehooptopbeter');
+            }
+
+    		if (count($missingfields) == 0) {
+
+                $donation = new Donation(0, $donate_amount_parsed, $donate_email, $donate_name, $donate_message, '', $donate_no_amount, $donate_anonymous);
+
+            }
+
+        }
+
 ?>	<section class="content">
 		<div class="sitewidth clearfix">
 
@@ -12,38 +52,45 @@
                 <form action="" method="post">
                     
                     <p><?php _e('Enter your details and the amount you want to donate below. Optionally you can make your donation anonymously or hide the amount of money you donate.', 'martinehooptopbeter'); ?>
+
+                    <?php if (count($missingfields) > 0) : ?>
+                        <p class="error"><?php _e('One or more fields are not filled in or incorrect. Please check and correct the entered data.', 'martinehooptopbeter'); ?>
+                    <?php endif ?>
+
                     <fieldset>
-                        <p>
+                        <p class="<?php if (isset($missingfields['donate_name'])) { echo 'error'; } ?>">
                             <label for="donate_name"><?php _e('Your name', 'martinehooptopbeter'); ?></label>
-                            <input type="text" class="textinput" id="donate_name" name="donate_name" value="" />
+                            <input type="text" class="textinput" id="donate_name" name="donate_name" value="<?php echo esc_attr($donate_name); ?>" />
                         </p>
-                        <p>
+                        <p class="<?php if (isset($missingfields['donate_email'])) { echo 'error'; } ?>">
                             <label for="donate_email"><?php _e('Your E-mail address', 'martinehooptopbeter'); ?></label>
-                            <input type="email" class="textinput" id="donate_email" name="donate_email" value="" placeholder="<?php _e('youremail@domain.com', 'martinehooptopbeter') ?>" />
+                            <input type="email" class="textinput" id="donate_email" name="donate_email" value="<?php echo esc_attr($donate_email); ?>" placeholder="<?php _e('youremail@domain.com', 'martinehooptopbeter') ?>" />
                         </p>
+                        <ul>
+                            <li><input type="checkbox" class="checkbox" id="donate_anonymous" name="donate_anonymous"<?php if ($donate_anonymous) { echo ' checked="checked"'; } ?> /><label for="donate_anonymous"><?php _e('I want to remain anonymous, do not show my name on the website.', 'martinehooptopbeter'); ?></label></li>
+                        </ul>
                         <p><?php _e('Do you want to support Martine?  Leave her a message that will show up on the website.', 'martinehooptopbeter'); ?></p>
-                        <p>
+                        <p class="<?php if (isset($missingfields['donate_message'])) { echo 'error'; } ?>">
                             <label for="donate_name"><?php _e('Message', 'martinehooptopbeter'); ?></label>
-                            <textarea id="donate_message" name="donate_message" rows="10"></textarea>
+                            <textarea id="donate_message" name="donate_message" rows="10"><?php echo esc_attr($donate_message); ?></textarea>
                         </p>
                     </fieldset>
 
                     <p><?php _e('Enter the amount you want to donate and choose your prefered payment method. You can donate immediately online.', 'martinehooptopbeter'); ?>
                     <fieldset>
-                        <p>
-                            <label for="donate_name"><?php _e('Amount to donate', 'martinehooptopbeter'); ?></label>
-                            <span>&euro; </span><input type="text" class="textinput numberinput clearnone" id="donate_email" name="donate_email" value="" placeholder="<?php _e('00.00', 'martinehooptopbeter') ?>"/>
-                        </p>
-                        <p>
-                            <label><?php _e('Payment method', 'martinehooptopbeter'); ?></label>
-                            <ul>
-                                <li><input type="radio" class="radio" id="donate_payment_method_ideal" name="donate_payment_method" value="ideal"><label for="donate_payment_method_ideal"><?php _e('iDEAL', 'martinehooptopbeter'); ?></label></li>
-                                <li><input type="radio" class="radio" id="donate_payment_method_creditcard" name="donate_payment_method" value="creditcard"><label for="donate_payment_method_creditcard"><?php _e('Credit Card', 'martinehooptopbeter'); ?></label></li>
-                            </ul>
+                        <p class="<?php if (isset($missingfields['donate_amount'])) { echo 'error'; } ?>">
+                            <label for="donate_amount"><?php _e('Amount to donate', 'martinehooptopbeter'); ?></label>
+                            <span>&euro; </span><input type="text" class="textinput numberinput clearnone" id="donate_amount" name="donate_amount" value="<?php echo esc_attr($donate_amount); ?>" placeholder="<?php _e('00.00', 'martinehooptopbeter') ?>"/>
                         </p>
                         <ul>
-                            <li><input type="checkbox" class="checkbox" id="donate_anonymous" value="" /><label for="donate_anonymous"><?php _e('I want to remain anonymous, do not show my name on the website.', 'martinehooptopbeter'); ?></label></li>
-                            <li><input type="checkbox" class="checkbox" id="donate_no_amount" value="" /><label for="donate_no_amount"><?php _e('Do not show the amount that I donate on the website.', 'martinehooptopbeter'); ?></label></li>
+                            <li><input type="checkbox" class="checkbox" id="donate_no_amount" name="donate_no_amount"<?php if ($donate_no_amount) { echo ' checked="checked"'; } ?> /><label for="donate_no_amount"><?php _e('Do not show the amount that I donate on the website.', 'martinehooptopbeter'); ?></label></li>
+                        </ul>
+                        <p class="<?php if (isset($missingfields['donate_payment_method'])) { echo 'error'; } ?>">
+                            <label><?php _e('Payment method', 'martinehooptopbeter'); ?></label>
+                        </p>
+                        <ul class="<?php if (isset($missingfields['donate_payment_method'])) { echo 'error'; } ?>">
+                            <li><input type="radio" class="radio" id="donate_payment_method_ideal" name="donate_payment_method" value="ideal"<?php if ($donate_payment_method == 'ideal') { echo ' checked="checked"'; } ?>><label for="donate_payment_method_ideal"><?php _e('iDEAL', 'martinehooptopbeter'); ?></label></li>
+                            <li><input type="radio" class="radio" id="donate_payment_method_creditcard" name="donate_payment_method" value="creditcard"<?php if ($donate_payment_method == 'creditcard') { echo ' checked="checked"'; } ?>><label for="donate_payment_method_creditcard"><?php _e('Credit Card', 'martinehooptopbeter'); ?></label></li>
                         </ul>
                     </fieldset>
 
@@ -60,6 +107,15 @@
 
 <?php
 
+    }
+
+    function parseAmount($amount) {
+        $amount = str_replace(',', '.', $amount);
+        if (is_numeric($amount)) {
+            $amound_parsed = floatval($amount);
+            return (int)($amound_parsed * 100);
+        }
+        return 0;
     }
 
 ?>
