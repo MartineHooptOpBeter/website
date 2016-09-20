8d8700326776a377385dbfcb806a5692c65069db
Marco Miltenburg <marco@virtualpages.nl>
2016-09-20 10:19:15 +0200

    Voorbeeld configuratiebestand toegevoegd.

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+
+    // ====================================================================
+    // Voorbeeld configuratiebestand
+    // Kopieer dit bestand naar de theme folder en pas de configuratie aan.
+    // ====================================================================
+
+    $config  = [];
+
+
+    // ========
+    // DONATIES
+    // ========
+
+    /* Database data source name */
+    $config['donate_dsn'] = '';
+
+    /* Gebruikersnaam om in te loggen op database server */
+    $config['donate_username'] = '';
+
+    /* Wachtwoord om in te loggen op database server */
+    $config['donate_password'] = '';
+
+?>
