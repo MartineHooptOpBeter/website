ab572f6e4152547a125d1d95d47d0efecd30d6fa
Marco Miltenburg <marco@virtualpages.nl>
2016-09-23 18:23:04 +0200

    Emoji support uitgeschakeld.

--- src/functions.php
+++ src/functions.php
@@ -10,6 +10,12 @@
 		
 		// Remove Wordpress generator in the head
 		remove_action('wp_head', 'wp_generator');
+		
+		// Remove Wordpress emoji support
+		remove_action('wp_head', 'print_emoji_detection_script', 7);
+		remove_action('wp_print_styles', 'print_emoji_styles');
+		remove_action('admin_print_scripts', 'print_emoji_detection_script');
+		remove_action('admin_print_styles', 'print_emoji_styles');
 
 		// Make theme available for translation
 		// Translations can be filed in the /languages/ directory
