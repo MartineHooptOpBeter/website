248e9239a4dcb81ea63be1bcd67865bc5e9fe545
Marco Miltenburg <marco@virtualpages.nl>
2016-09-23 18:20:38 +0200

    Onnodige stylesheet weggehaald.

--- src/header.php
+++ src/header.php
@@ -2,7 +2,6 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-	<link rel="stylesheet" href="style.css" type="text/css" />
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="robots" content="index,follow" />
