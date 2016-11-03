fc90101c6bf233a9742b5783549934371003a378
Marco Miltenburg <marco@virtualpages.nl>
2016-11-03 09:29:36 +0100

    Ondersteuning toegevoegd voor het correct tonen van de WP Admin Bar.

--- src/footer.php
+++ src/footer.php
@@ -3,6 +3,8 @@
 		<p class="small"><a href="<%= release.homepage %>" target="_blank"><?php echo vsprintf(esc_attr(__('Source code available on %1$s', 'martinehooptopbeter')), '<i class="icon icon-github"></i>GitHub'); ?></a></p>
 	</footer>
 
+<?php echo wp_footer(); ?>
+
 </body>
 
 <!--
