6efe73119ea1a360ae762323227d08744c4ff095
Marco Miltenburg <marco@virtualpages.nl>
2020-05-30 14:07:22 +0200

    Updated PHPMailer to version 6.1.6

--- src/utilities.class.php
+++ src/utilities.class.php
@@ -2,6 +2,10 @@
 
     @@HEADER@@
 
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\SMTP;
+	use PHPMailer\PHPMailer\Exception;
+
 	require_once 'vendor/autoload.php';
 
     class Utilities {
